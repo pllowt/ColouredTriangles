3535131e89855e767f17d8cb884de96f8aa0c5cf
Paul <paul.lowther01@hotmail.com>
2024-03-10 11:28:56 +0000

    Add map for each string builder value

--- ColouredTriangles.hs
+++ ColouredTriangles.hs
@@ -1,2 +1,7 @@
-triangle :: String -> String
-triangle x = map x!!1
+import qualified Data.Map as Map
+
+colourMap :: Map.Map (String, String, String) String
+colourMap = Map.fromList [
+    (("BB", "GR", "RG"), "B"),
+    (("BR", "RB", "GG"), "G"),
+    (("BG", "GB", "RR"), "R")]
